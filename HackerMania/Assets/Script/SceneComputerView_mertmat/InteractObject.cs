abae1b38a0fc5025c266b56974677bff6aaa1155
alexis-fresard <94693019+MathildeMertz@users.noreply.github.com>
2023-05-11 08:38:15 +0200

    modification de l'interaction
    souris interaction modif

--- HackerMania/Assets/Script/SceneComputerView_mertmat/InteractObject.cs
+++ HackerMania/Assets/Script/SceneComputerView_mertmat/InteractObject.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractObject : MonoBehaviour
-
 {
+    private bool triggerActive = false;
+
     void OnTriggerEnter(Collider other) {
-        if(Input.GetKeyDown(KeyCode.JoystickButton0) {
+            if (other.CompareTag("Player")) {
+                triggerActive = true;
+            }
+        }
+
+        void OnTriggerExit(Collider other) {
+           if (other.CompareTag("Player")) {
+               triggerActive = false;
+           }
+        }
+
+        private void Update() {
+             if (triggerActive && Input.GetKeyDown(KeyCode.JoystickButton0)) {
              Debug.Log("vous avez cliqué sur la touche");
         }
     }
