3f151a595924123ad3d31be6ec4c19212c523b64
alexis-fresard <94693019+MathildeMertz@users.noreply.github.com>
2023-05-25 08:13:54 +0200

    dernière modif

--- HackerMania/Assets/TextBoxManage.cs
+++ HackerMania/Assets/TextBoxManage.cs
@@ -1,4 +1,4 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,30 +14,27 @@ private bool isDialogueActive = false;
 void Start()
 {
 CanvasTextBox.SetActive(false); 
-  
 }
 
 public void OnClickButton ()
 {
-    if (!isDialogueActive)
-    {
-        isDialogueActive = true;
-        CanvasTextBox.SetActive(true);
-        DialogueText.text = Texte;
-        Debug.Log("Dialogue lancé");
-    }
+if (!isDialogueActive)
+{
+    isDialogueActive = true;
+    CanvasTextBox.SetActive(true);
+    DialogueText.text = Texte;
+    Debug.Log("Dialogue lancé");
+}
 }
 
 // Update is called once per frame
 void Update()
 {
-        if (Input.GetKeyDown(KeyCode.JoystickButton0) && isDialogueActive)
-        {
-            dialogueBox.SetActive(false); // désactive la boîte de dialogue lorsque la touche "Enter" est enfoncée
-            isDialogueActive = false;
-            playerMovement.enabled = true; // Réactiver le script de mouvement du joueur
-            Debug.Log("Dialogue terminé");
-        }
+    if (Input.GetKeyDown(KeyCode.JoystickButton0) && isDialogueActive) {
+        dialogueBox.SetActive(false); // désactive la boîte de dialogue lorsque la touche "Enter" est enfoncée
+        isDialogueActive = false;
+        playerMovement.enabled = true; // Réactiver le script de mouvement du joueur
+        Debug.Log("Dialogue terminé");
     }
 }
 }
