c6eb0a872005ff073665d82cf930da8621e9ed40
alexis-fresard <alexis.fresard@divtec.ch>
2023-05-26 09:42:44 +0200

    VirtualKeyboard quasi terminer

--- HackerMania/Assets/KeyInput.cs
+++ HackerMania/Assets/KeyInput.cs
@@ -7,11 +7,24 @@ public class KeyInput : MonoBehaviour
     public TMP_InputField inputField; // Référence vers votre InputField TMP
 
     public void AddKey()
+{
+    if (Input.GetKey(KeyCode.JoystickButton0))
     {
-        if (Input.GetKey(KeyCode.JoystickButton0))
+        string key = gameObject.name; // Récupère le nom du bouton (peut être utilisé pour déterminer la touche correspondante)
+        
+        if (key == "BackSpace")
         {
-            string key = gameObject.name; // Récupère le nom du bouton (peut être utilisé pour déterminer la touche correspondante)
-            inputField.text += key; // Ajoute la touche à la fin du texte actuel dans le InputField TMP
+            // Retire la dernière lettre du texte
+            if (inputField.text.Length > 0)
+            {
+                inputField.text = inputField.text.Substring(0, inputField.text.Length - 1);
+            }
+        }
+        else
+        {
+            // Ajoute la touche à la fin du texte actuel dans le InputField TMP
+            inputField.text += key;
         }
     }
+}
 }
