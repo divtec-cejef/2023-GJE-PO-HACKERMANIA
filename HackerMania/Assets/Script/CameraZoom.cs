5904b9e2663261a3102be9b8c8fc1fd85e2654ec
alexis-fresard <alexis.fresard@divtec.ch>
2023-06-20 10:35:03 +0200

    commit 20.06.2023
    commit : 10h34

--- HackerMania/Assets/Script/CameraZoom.cs
+++ HackerMania/Assets/Script/CameraZoom.cs
@@ -1,29 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraZoom : MonoBehaviour
 {
-
     private Camera cam;
     private float targetZoom;
     private float zoomFactor = 3f;
-    private float zoomLerpSpeed = 10;
+    private float zoomLerpSpeed = 10f;
+    private float joystickZoomSpeed = 2f; // Vitesse de recul avec le joystick
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         cam = Camera.main;
         targetZoom = cam.orthographicSize;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        float scrollData;
-        scrollData =Input.GetAxis("Mouse ScrollWheel");
+        float scrollData = Input.GetAxis("Mouse ScrollWheel");
 
-        targetZoom -= scrollData * zoomFactor;
+        // Ajouter l'input de la molette de la souris pour le zoom
+        float zoomInput = scrollData * zoomFactor;
+        
+        // Zoom la caméra avec Joystickbutton5
+        if (Input.GetKeyDown(KeyCode.JoystickButton5))
+        {
+            zoomInput += joystickZoomSpeed;
+        }
+        
+        // Dézoom la caméra avec Joystickbutton4
+        if (Input.GetKeyDown(KeyCode.JoystickButton4))
+        {
+            zoomInput -= joystickZoomSpeed;
+        }
+
+        targetZoom -= zoomInput;
         targetZoom = Mathf.Clamp(targetZoom, 4.5f, 18f);
         cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.deltaTime * zoomLerpSpeed);
     }
