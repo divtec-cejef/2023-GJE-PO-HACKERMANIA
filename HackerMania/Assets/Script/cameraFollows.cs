795ca3826afa09bc30bfc35b59cfb056e46c91fc
alexis-fresard <alexis.fresard@divtec.ch>
2023-08-22 13:24:02 +0200

    Mise en commun des trois codes

---
+++ HackerMania/Assets/Script/cameraFollows.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class cameraFollows : MonoBehaviour
+
+{
+    public GameObject player;
+    public float timeOffset;
+    public Vector3 posOffset;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
